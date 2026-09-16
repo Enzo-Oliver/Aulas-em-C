9a641dc180cf02cb97a2ff46483c00f89e1ca1cf
Enzo-Oliver <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>
2026-09-16 12:28:23 -0300

    Aula10

---
+++ explicaçao_funcao.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+
+// float media(float a, float b){
+//     float media;
+//     media = (a + b) / 2;
+//     return media;
+// }
+
+// int main(){
+
+//     float m = media(10, 5);
+//     printf("A media eh: %.2f\n", m);
+//     return 0;
+//
